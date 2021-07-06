9c021d7d766b6186636b4f99fda7884355b6963a
cebsec17 <cebsec17@htl-kaindorf.at>
2021-07-06 15:35:07 +0200

    Navigation Admin 3

---
+++ app/src/main/java/com/example/navigation/Login.java
@@ -0,0 +1,92 @@
+package com.example.navigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Login extends Activity {
+
+    Button b1,b2;
+    EditText etUsername, etPassword;
+
+    private String username, password;
+    private final String URL = "http://172.28.57.24/login/login.php";
+
+    private static final String TAG = "MyActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        username = password = "";
+
+        b1 = findViewById(R.id.button);
+        etUsername = findViewById(R.id.username);
+        etPassword = findViewById(R.id.password);
+
+        b2 = findViewById(R.id.button2);
+
+        b2.setOnClickListener(v -> finish());
+    }
+
+    public void login(View view){
+        username = etUsername.getText().toString();
+        password = etPassword.getText().toString();
+
+        Log.i(TAG, ""+username);
+        Log.i(TAG, ""+password);
+
+        if(!username.equals("") && !password.equals("")){
+            Log.i(TAG, "1"+username);
+            Log.i(TAG, "1"+password);
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, response -> {
+
+                Log.i(TAG, "2"+username);
+                Log.i(TAG, "2"+password);
+                Log.i(TAG, ""+response);
+
+                if (response.equals("Success")) {
+
+                    Log.i(TAG, "Bis hier geht es");
+                    Intent intent = new Intent(this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else if (response.equals("Failure")) {
+                    Toast.makeText(this, "Invalid Login", Toast.LENGTH_SHORT).show();
+                }
+            }, error -> Toast.makeText(this, error.toString().trim(), Toast.LENGTH_SHORT).show())
+            {
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> data = new HashMap<>();
+                    data.put("username", username);
+                    data.put("password", password);
+                    return data;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(stringRequest);
+        }
+        else
+        {
+            Log.i(TAG, "Kein Password");
+            Toast.makeText(this, "Fields can not be empty!", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
