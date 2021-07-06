640d71799a761fd8148691e0e76b442e88d4474f
cebsec17 <cebsec17@htl-kaindorf.at>
2021-07-06 15:24:22 +0200

    Navigation Admin 2

--- app/src/main/java/com/example/navigation/MainActivity.java
+++ app/src/main/java/com/example/navigation/MainActivity.java
@@ -1,20 +1,16 @@
 package com.example.navigation;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
                     replace(new ProfileFragment());
                     break;
             }
-
             return true;
         });
     }
