df590cb44ed310199007b9f7e8e716e732c05a86
weiluc17 <weiluc17@htl-kaindorf.at>
2021-07-07 13:53:34 +0200

    Barcode Scanner 7.7.2021

---
+++ app/src/main/java/com/example/navigation/Details.java
@@ -0,0 +1,32 @@
+package com.example.navigation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Details extends AppCompatActivity {
+
+    private TextView barcodeText;
+    private TextView barcodeType;
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.details);
+
+        barcodeText = findViewById(R.id.barcode_text);
+        barcodeType = findViewById(R.id.barcode_type);
+
+
+        Integer barcodeTyp = getIntent().getIntExtra("barcodeTyp",0);
+        String barcodeData = getIntent().getStringExtra("barcodeData");
+        barcodeText.setText(barcodeData);
+        barcodeType.setText(Integer.toString(barcodeTyp));
+
+    }
+
+}
+
